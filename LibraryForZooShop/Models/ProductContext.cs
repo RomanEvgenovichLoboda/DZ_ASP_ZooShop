717b628107fe80d8000049030f8b7a4120146456
loboda.rom@gmail.com <loboda.rom@gmail.com>
2022-10-19 20:13:28 +0300

    publishOnHost

--- LibraryForZooShop/Models/ProductContext.cs
+++ LibraryForZooShop/Models/ProductContext.cs
@@ -9,11 +9,12 @@ namespace DZ_ASP_ZooShop.Models
         public DbSet<Animal> Animals { get; set; }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            new ConfigurationBuilder().AddUserSecrets<ProductContext>()
-                .Build()
-                .Providers
-                .First()
-                .TryGet("connStr", out var connStr);
+            //new ConfigurationBuilder().AddUserSecrets<ProductContext>()
+            //    .Build()
+            //    .Providers
+            //    .First()
+            //    .TryGet("connStr", out var connStr);
+            string connStr = "Data Source=SQL5105.site4now.net;Initial Catalog=db_a8dfed_mydatabase1;User Id=db_a8dfed_mydatabase1_admin;Password=2657sgnusmas";
             optionsBuilder.UseSqlServer(connStr);
         }
     }
